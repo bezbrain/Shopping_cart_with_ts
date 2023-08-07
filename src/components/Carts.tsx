bde9b35a484e66c83b26a52a28591b2c5b63d4f1
bezbrain <belloquadriolawale@gmail.com>
2023-08-07 08:10:28 -0700

    Populated with other data

--- src/components/Carts.tsx
+++ src/components/Carts.tsx
@@ -1,11 +1,14 @@
 import SingleCart from "./SingleCart";
 import cartStyles from "../styles/Cart.module.css";
+import { data } from "../data";
 
 const Carts = () => {
   return (
     <main className={cartStyles.main}>
       <p>YOUR BAG</p>
-      <SingleCart />
+      {data.map((each) => {
+        return <SingleCart key={each.id} {...each} />;
+      })}
     </main>
   );
 };
--- src/components/SingleCart.tsx
+++ src/components/SingleCart.tsx
@@ -1,25 +1,31 @@
 import { ChevronDown, ChevronUp } from "../icons/icons";
 import cartStyles from "../styles/Cart.module.css";
+// import { Cart } from "../../model";
 
-const SingleCart = () => {
+interface Cart {
+  id: number;
+  count: number;
+  img: string;
+  brand: string;
+  price: string;
+}
+
+const SingleCart: React.FC<Cart> = ({ img, count, brand, price }) => {
   return (
     <section>
       <div className={cartStyles.imgCon}>
-        <img
-          src="https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681753003/react-cart-project/phone-1_e5pkao.png"
-          alt="Phone"
-        />
+        <img src={img} alt="Phone" />
       </div>
       <div className={cartStyles.desc}>
-        <h3>Samsung Galaxy S8</h3>
-        <p>$399.99</p>
+        <h3>{brand}</h3>
+        <p>${price}</p>
         <button>remove</button>
       </div>
       <div className={cartStyles.count}>
         <div className={cartStyles.icon}>
           <ChevronUp />
         </div>
-        <p>1</p>
+        <p>{count}</p>
         <div className={cartStyles.icon}>
           <ChevronDown />
         </div>
